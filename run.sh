1235439a836438029506deef0849090700997549
Hongbo Ma <154826089+Ma-Hongbo@users.noreply.github.com>
2025-11-23 14:12:29 +0800

    Add script to run panorama SFM with conda

---
+++ run.sh
@@ -0,0 +1,3 @@
+source /lustre/fs12/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.sh
+conda activate colmap
+python /lustre/fsw/portfolios/nvr/users/ymingli/projects/colmap/python/examples/panorama_sfm.py --input_image_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket/Supermarket/sample_5 --output_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket/Supermarket/sample_5_process
