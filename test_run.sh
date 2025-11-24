8ef89da0bcfecf691f1690666185d30db14c04da
Hongbo Ma <154826089+Ma-Hongbo@users.noreply.github.com>
2025-11-24 14:04:39 +0800

    Update input and output paths in test_run.sh

---
+++ test_run.sh
@@ -0,0 +1,3 @@
+source /lustre/fs12/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.sh
+conda activate colmap
+python /lustre/fsw/portfolios/nvr/users/ymingli/projects/mhb/citygaussian/colmap/python/examples/panorama_sfm.py --input_image_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket/Supermarket/test --output_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket/Supermarket/test_process --matcher vocabtree
