2c47770347bd1d1bc29d760132e160bc8b431335
Hongbo Ma <154826089+Ma-Hongbo@users.noreply.github.com>
2025-11-26 23:02:18 +0800

    Add run_render.sh to execute render.py script

---
+++ run_render.sh
@@ -0,0 +1,2 @@
+
+python /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/projects/mhb/citygaussian/Grendel-GS/render.py -s /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/datasets/supermarket/Supermarket/sample_5_process --model_path /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/projects/mhb/citygaussian/Grendel-GS/sample_5 --bsz 128
