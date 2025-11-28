a7a9c934a45fae7e6bb8b9e3053e5cbc13aa47ee
Hongbo Ma <154826089+Ma-Hongbo@users.noreply.github.com>
2025-11-29 02:21:41 +0800

    Update dataset paths in run_render.sh

--- run_render.sh
+++ run_render.sh
@@ -1,3 +1,3 @@
 source /lustre/fs12/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.sh
 conda activate gaussian_splatting
-python /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/projects/mhb/citygaussian/Grendel-GS/render.py -s /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/datasets/supermarket/sample_3_process --model_path /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/datasets/supermarket/sample_3_result --bsz 128
+python /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/projects/mhb/citygaussian/Grendel-GS/render.py -s /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/datasets/supermarket/sample_5_process --model_path /lustre/fs12/portfolios/nvr/projects/nvr_av_end2endav/users/ymingli/datasets/supermarket/sample_5_result --bsz 128
