60d3f75ef89c36f5ae4f9205fc380c73cfc75390
Hongbo Ma <154826089+Ma-Hongbo@users.noreply.github.com>
2025-11-30 01:13:56 +0800

    Set KMP_DUPLICATE_LIB_OK and change OPENBLAS_NUM_THREADS

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-export OPENBLAS_NUM_THREADS=4
+export KMP_DUPLICATE_LIB_OK=TRUE
+export OPENBLAS_NUM_THREADS=1
 source /lustre/fs12/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.sh
 conda activate pycolmap
 python /lustre/fsw/portfolios/nvr/users/ymingli/projects/mhb/citygaussian/colmap/python/examples/panorama_sfm.py --input_image_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket_middle/sample_3 --output_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket_middle/sample_3_process --matcher vocabtree
